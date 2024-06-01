14193769c470308b36c3e21addf3c7740c8dae72
peanch <s695391353@gmail.com>
2024-06-01 23:10:04 +0800

    file to reproduce all at once

---
+++ code/reproduce.r
@@ -0,0 +1,10 @@
+# This file is used for repruducing all results in `analysis.Qmd` at once
+# Packages needed are listed as below
+
+library(tidyverse) # data cleaning
+library(knitr) # display tables in good format
+library(caret) # confusion matrics
+library(bnclassify) # BN
+library(kernlab) # SVM
+library(randomForest) # random forest
+library(C50) # C5.0 decision tree
